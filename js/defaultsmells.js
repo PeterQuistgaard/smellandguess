75e301dfcb38cc742d17d58406c22d2eee82e3f8
Peter Quistgaard <peter_quistgaard@hotmail.com>
2025-03-15 13:23:22 +0100

    Add files via upload

--- js/defaultsmells.js
+++ js/defaultsmells.js
@@ -1,55 +1,318 @@
 
-const defaultsmells = [
-  { id: 0, name: "Hindbær" },
-  { id: 1, name: "Lime" },
-  { id: 2, name: "Timian" },
+
+const defaultsmells3 = [
+  { id: 0, name: "Hindbær",
+    languages: {
+      "da": "Hindbær",
+      "sv": "Hallon",
+      "fi": "Vadelma",
+      "en": "Raspberry",
+      "es": "Frambuesa"
+    }
+  },
+  { id: 1, name: "Lime"},
+  {
+    id: 2, name: "Timian",
+    languages: {
+      "da": "Timian",
+      "sv": "Timjansläktet",
+      "fi": "Ajuruohot",
+      "en": "Thymus",
+      "es": "Tomillo"
+    }
+  },
   { id: 3, name: "Eddike" },
   { id: 4, name: "Karry" },
   { id: 5, name: "Karamel" },
   { id: 6, name: "Ananas" },
   { id: 7, name: "Pebermynte" },
-  { id: 8, name: "Kamille" },
+  { id: 8, name: "Kamille",
+    languages: {
+      "da": "Kamille",
+      "sv": "Kamomill",
+      "fi": "Kamomilla",
+      "en": "Camomile",
+      "es": "Manzanilla"
+    }
+   },
   { id: 9, name: "Kirsebær" },
   { id: 10, name: "Abrikos" },
-  { id: 11, name: "Kardemomme" },
-  { id: 12, name: "Allehånde" },
-  { id: 13, name: "Koriander" },
-  { id: 14, name: "Spidskommen" },
-  { id: 15, name: "Kaffe" },
-   { id: 16, name: "Kanel" },
-  { id: 17, name: "Mandel" },
-  { id: 18, name: "Vanilje" },
-  { id: 19, name: "Nellike" },
-  { id: 20, name: "Rosmarin" },
-  { id: 21, name: "Muskat" },
-  { id: 22, name: "Ingefær" },
-  { id: 23, name: "Cayenne" },
+  {
+    id: 11, name: "Kardemomme",
+    languages: {
+      "da": "Kardemomme",
+      "sv": "Kardemumma",
+      "fi": "Kardemumma",
+      "en": "Cardamom",
+      "es": "Cardamomo"
+    }
+  },
+  {
+    id: 12, name: "Allehånde",
+    languages: {
+      "da": "Allehånde",
+      "sv": "Kryddpeppar",
+      "fi": "Maustepippuri",
+      "en": "Allspice",
+      "es": "Pimienta de Jamaica"
+    }
+  },
+  {
+    id: 13, name: "Koriander",
+    languages: {
+      "da": "Koriander",
+      "sv": "Koriander",
+      "fi": "Korianteri",
+      "en": "Coriander",
+      "es": "Cilantro"
+    }
+  },
+  { id: 14, name: "Spidskommen",
+    languages: {
+      "da": "Spidskommen",
+      "sv": "Spiskummin",
+      "fi": "Roomankumina",
+      "en": "Cumin",
+      "es": "Comino"
+    } },
+  { id: 15, name: "Kaffe",
+    languages: {
+      "da": "Kaffe",
+      "sv": "Kaffe",
+      "fi": "Kahvi",
+      "en": "Coffee",
+      "es": "Café"
+    } },
+  {
+    id: 16, name: "Kanel",
+    languages: {
+      "da": "Kanel",
+      "sv": "Kanel",
+      "fi": "Kaneli",
+      "en": "Cinnamon",
+      "es": "Canela"
+    }
+  },
+  {
+    id: 17, name: "Mandel",
+    languages: {
+      "da": "Mandel",
+      "sv": "Mandel",
+      "fi": "Manteli",
+      "en": "Almond",
+      "es": "Almendra"
+    }
+  },
+  {
+    id: 18, name: "Vanilje",
+    languages: {
+      "da": "Vanilje",
+      "sv": "Vanilj",
+      "fi": "Vaniljat",
+      "en": "Vanilla",
+      "es": "Vainilla"
+    }
+  },
+  {
+    id: 19, name: "Nellike",
+    languages: {
+      "da": "Nellike",
+      "sv": "Nejlika",
+      "fi": "Neilikka",
+      "en": "Clove ",
+      "es": "Clavel"
+    }
+  },
+  {
+    id: 20, name: "Rosmarin",
+    languages: {
+      "da": "Rosmarin",
+      "sv": "Rosmarin",
+      "fi": "Rosmariini",
+      "en": "Rosemary",
+      "es": "Romero"
+    }
+  },
+  {
+    id: 21, name: "Muskatnød",
+    languages: {
+      "da": "Muskatnød",
+      "sv": "Muskot",
+      "fi": "Muskottipähkinä",
+      "en": "Nutmeg ",
+      "es": "Nuez Moscada"
+    }
+  },
+  {
+    id: 22, name: "Ingefær",
+    languages: {
+      "da": "Ingefær",
+      "sv": "Ingefära",
+      "fi": "Inkivääri",
+      "en": "Ginger",
+      "es": "Jengibre"
+    }
+  },
+  {
+    id: 23, name: "Cayennepeber",
+    languages: {
+      "da": "Cayennepeber",
+      "sv": "Cayennepeppar",
+      "fi": "Cayennepippuri",
+      "en": "Cayenne pepper",
+      "es": "Pimienta de Cayena"
+    }
+  },
   { id: 24, name: "Anis" },
   { id: 25, name: "Banan" },
   { id: 26, name: "Kokos" },
   { id: 27, name: "Peanuts" },
   { id: 28, name: "Mandarin" },
   { id: 29, name: "Jordbær" },
-  { id: 30, name: "Estragon" },
-  { id: 31, name: "Peber" }, ,
-  { id: 32, name: "Basilikum" },
-  { id: 33, name: "Enebær" },
-  { id: 34, name: "Cirton" },
+  { id: 30, name: "Estragon",
+    languages: {
+      "da": "Estragon",
+      "sv": "Dragon",
+      "fi": "Rakuuna",
+      "en": "Tarragon",
+      "es": "Estragón"
+    } },
+  { id: 31, name: "Peber",
+    languages: {
+      "da": "Peber",
+      "sv": "Peppar",
+      "fi": "Pippuri",
+      "en": "Pepper",
+      "es": "Pimienta"
+    }  },
+  { id: 32, name: "Basilikum" ,
+    languages: {
+      "da": "Basilikum",
+      "sv": "Basilika",
+      "fi": "Maustebasilika",
+      "en": "Basil",
+      "es": "Albahaca"
+    } },
+  { id: 33, name: "Enebær" ,
+    languages: {
+      "da": "Enebær",
+      "sv": "Juniper",
+      "fi": "Kataja",
+      "en": "Juniper",
+      "es": "Enebro"
+    } },
+  { id: 34, name: "Citron" },
   { id: 35, name: "Eucalyptus" },
   { id: 36, name: "Appelsin" },
   { id: 37, name: "Lavendel" },
   { id: 38, name: "Grape" },
-  { id: 39, name: "Rose" }, 
-  { id:40 , name: "Kamfer" },
-  { id:41 , name: "Cedertræ" },
-  { id:42 , name: "Gran" },
+  { id: 39, name: "Rose" },
+  { id: 40, name: "Kamfer" },
+  { id: 41, name: "Cedertræ" },
+  { id: 42, name: "Gran" },
 ];
 
 
-/*
-kamfer, cedertræ, gran
- { id:40 , name: "Kamfer" },
- { id:41 , name: "Cedertræ" },
- { id:42 , name: "Gran" },
- { id: , name: "" },
-*/
+
+// const defaultsmells2 = {
+//   da: [
+//     { id: 0, name: "Hindbær" },
+//     { id: 1, name: "Lime" },
+//     { id: 2, name: "Timian Da" },
+//     { id: 3, name: "Eddike" },
+//     { id: 4, name: "Karry" },
+//     { id: 5, name: "Karamel" },
+//     { id: 6, name: "Ananas" },
+//     { id: 7, name: "Pebermynte" },
+//     { id: 8, name: "Kamille" },
+//     { id: 9, name: "Kirsebær" },
+//     { id: 10, name: "Abrikos" },
+//     { id: 11, name: "Kardemomme" },
+//     { id: 12, name: "Allehånde" },
+//     { id: 13, name: "Koriander" },
+//     { id: 14, name: "Spidskommen" },
+//     { id: 15, name: "Kaffe" },
+//     { id: 16, name: "Kanel" },
+//     { id: 17, name: "Mandel" },
+//     { id: 18, name: "Vanilje" },
+//     { id: 19, name: "Nellike" },
+//     { id: 20, name: "Rosmarin" },
+//     { id: 21, name: "Muskat" },
+//     { id: 22, name: "Ingefær" },
+//     { id: 23, name: "Cayenne" },
+//     { id: 24, name: "Anis" },
+//     { id: 25, name: "Banan" },
+//     { id: 26, name: "Kokos" },
+//     { id: 27, name: "Peanuts" },
+//     { id: 28, name: "Mandarin" },
+//     { id: 29, name: "Jordbær" },
+//     { id: 30, name: "Estragon" },
+//     { id: 31, name: "Peber" }, ,
+//     { id: 32, name: "Basilikum" },
+//     { id: 33, name: "Enebær" },
+//     { id: 34, name: "Citron" },
+//     { id: 35, name: "Eucalyptus" },
+//     { id: 36, name: "Appelsin" },
+//     { id: 37, name: "Lavendel" },
+//     { id: 38, name: "Grape" },
+//     { id: 39, name: "Rose" },
+//     { id: 40, name: "Kamfer" },
+//     { id: 41, name: "Cedertræ" },
+//     { id: 42, name: "Gran" },
+//   ]
+//   ,
+//   en: {
+//     "ka": "Curry"
+//   }
+
+// }
+
+
+
+// const defaultsmells = [
+//   { id: 0, name: "Hindbær" },
+//   { id: 1, name: "Lime" },
+//   { id: 2, name: "Timian" },
+//   { id: 3, name: "Eddike" },
+//   { id: 4, name: "Karry" },
+//   { id: 5, name: "Karamel" },
+//   { id: 6, name: "Ananas" },
+//   { id: 7, name: "Pebermynte" },
+//   { id: 8, name: "Kamille" },
+//   { id: 9, name: "Kirsebær" },
+//   { id: 10, name: "Abrikos" },
+//   { id: 11, name: "Kardemomme" },
+//   { id: 12, name: "Allehånde" },
+//   { id: 13, name: "Koriander" },
+//   { id: 14, name: "Spidskommen" },
+//   { id: 15, name: "Kaffe" },
+//   { id: 16, name: "Kanel" },
+//   { id: 17, name: "Mandel" },
+//   { id: 18, name: "Vanilje" },
+//   { id: 19, name: "Nellike" },
+//   { id: 20, name: "Rosmarin" },
+//   { id: 21, name: "Muskat" },
+//   { id: 22, name: "Ingefær" },
+//   { id: 23, name: "Cayenne" },
+//   { id: 24, name: "Anis" },
+//   { id: 25, name: "Banan" },
+//   { id: 26, name: "Kokos" },
+//   { id: 27, name: "Peanuts" },
+//   { id: 28, name: "Mandarin" },
+//   { id: 29, name: "Jordbær" },
+//   { id: 30, name: "Estragon" },
+//   { id: 31, name: "Peber" }, ,
+//   { id: 32, name: "Basilikum" },
+//   { id: 33, name: "Enebær" },
+//   { id: 34, name: "Citron" },
+//   { id: 35, name: "Eucalyptus" },
+//   { id: 36, name: "Appelsin" },
+//   { id: 37, name: "Lavendel" },
+//   { id: 38, name: "Grape" },
+//   { id: 39, name: "Rose" },
+//   { id: 40, name: "Kamfer" },
+//   { id: 41, name: "Cedertræ" },
+//   { id: 42, name: "Gran" },
+// ];
+
+
